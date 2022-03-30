b12eb8307e6ddac200db658467730f19d72b5113
roroism <95632580+roroism@users.noreply.github.com>
2022-03-30 23:57:16 +0900

    데스크탑 전용 기업형 웹 프로젝트

--- lib/dbconn.php
+++ lib/dbconn.php
@@ -5,10 +5,39 @@
 
     mysql_select_db("www_db",$connect);
 
+
+
+    // $connect = new mysqli($servername, $username, $password, $dbname);
+    // $connect = new mysqli("localhost", "www", "1234", "www_db");
+    // // Check connection
+    // if ($connect->connect_error) {
+    //     die("Connection failed: " . $connect->connect_error);
+    // }
+
+
+
+
+
+
+
+
+
+
     //server
     // $connect=mysql_connect( "localhost", "roro312", "Qhfhfh312") or  
     //     die( "SQL server에 연결할 수 없습니다."); 
 
     // mysql_select_db("roro312",$connect);
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
